96cc8e47cf1083a791e7f9415f7ce74db537080d
nagendragpu <nagendragpu@gmail.com>
2021-07-30 02:29:49 +0530

    BookNow button Implementation

--- Screens/DetailsScreen.js
+++ Screens/DetailsScreen.js
@@ -1,10 +1,61 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 
-const DetailsScreen = () => {
+const DetailsScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity
+        style={{ position: "absolute", top: 80, left: 20 }}
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "#fefeff",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 20,
+            height: 30,
+            width: 30,
+          }}
+        >
+          <Image
+            source={require("../assets/love.png")}
+            style={{
+              height: 20,
+              width: 15,
+              resizeMode: "center",
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          backgroundColor: "#120c46",
+          bottom: 20,
+          left: 25,
+          right: 25,
+          borderRadius: 30,
+          height: 60,
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontWeight: "700",
+            fontSize: 18,
+            color: "white",
+          }}
+        >
+          Book Now
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
