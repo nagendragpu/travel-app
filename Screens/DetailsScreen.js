7f3a0db3eadbb15398dfa11db1c1149c5699b2b5
nagendragpu <nagendragpu@gmail.com>
2021-07-30 13:46:43 +0530

    DetailScreen Header Image

--- Screens/DetailsScreen.js
+++ Screens/DetailsScreen.js
@@ -1,36 +1,127 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { Dimensions } from "react-native";
 
 const DetailsScreen = ({ navigation }) => {
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
   return (
-    <View style={{ flex: 1 }}>
-      <TouchableOpacity
-        style={{ position: "absolute", top: 80, left: 20 }}
-        onPress={() => {
-          navigation.navigate("Home");
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        width: "100%",
+      }}
+    >
+      <View
+        style={{
+          height: windowHeight * 0.5,
+          margin: 10,
+          marginTop: 0,
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+
+          overflow: "hidden",
         }}
       >
-        <View
-          style={{
-            backgroundColor: "#fefeff",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 20,
-            height: 30,
-            width: 30,
-          }}
+        <ImageBackground
+          style={{ flex: 1 }}
+          source={require("../assets/landscape.png")}
+          resizeMode="cover"
         >
-          <Image
-            source={require("../assets/love.png")}
+          <View
             style={{
-              height: 20,
-              width: 15,
-              resizeMode: "center",
+              marginTop: 60,
+              marginHorizontal: 25,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
-          />
-        </View>
-      </TouchableOpacity>
+          >
+            <TouchableOpacity
+              style={{ flex: 1 }}
+              onPress={() => {
+                navigation.navigate("Home");
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "#fefeff",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 20,
+                  height: 40,
+                  width: 40,
+                }}
+              >
+                <Image
+                  source={require("../assets/left.png")}
+                  style={{
+                    height: 20,
+                    width: 20,
+                    resizeMode: "center",
+                    tintColor: "black",
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginRight: 10 }} onPress={() => {}}>
+              <View
+                style={{
+                  backgroundColor: "#fefeff",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 20,
+                  height: 40,
+                  width: 40,
+                }}
+              >
+                <Image
+                  source={require("../assets/export.png")}
+                  style={{
+                    height: 20,
+                    width: 20,
+                    resizeMode: "center",
+                    tintColor: "black",
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{}} onPress={() => {}}>
+              <View
+                style={{
+                  backgroundColor: "#fefeff",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 20,
+                  height: 40,
+                  width: 40,
+                }}
+              >
+                <Image
+                  source={require("../assets/white-heart.png")}
+                  style={{
+                    height: 20,
+                    width: 20,
+                    resizeMode: "center",
+                    tintColor: "black",
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
 
       <TouchableOpacity
         style={{
