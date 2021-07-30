5b14093099098e949d96f2b92c18a5032411e14f
nagendragpu <nagendragpu@gmail.com>
2021-07-30 17:48:53 +0530

    MapDetails

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,10 +1,56 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import MapView from "react-native-maps";
 
-const Settings = () => {
+const Settings = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Setting!</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          position: "absolute",
+          top: 50,
+          left: 30,
+          right: 30,
+          zIndex: 100,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "#ffffff",
+          paddingVertical: 22,
+          paddingHorizontal: 20,
+          borderRadius: 20,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <Image
+            style={{ height: 25, width: 25, tintColor: "black" }}
+            source={require("../assets/left.png")}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            flex: 1,
+            textAlign: "center",
+            fontSize: 18,
+            fontWeight: "bold",
+          }}
+        >
+          Colosseum,Rome
+        </Text>
+      </View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{ height: "100%", width: "100%" }}
+      />
     </View>
   );
 };
